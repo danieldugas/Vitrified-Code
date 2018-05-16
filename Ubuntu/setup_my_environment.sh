cb23d5fbc7b55b6067dd01ea9868de88c0986ca5
Daniel <exodaniel@gmail.com>
2018-05-16 14:52:55 +0200

    Oneclickextract

--- Ubuntu/setup_my_environment.sh
+++ Ubuntu/setup_my_environment.sh
@@ -13,13 +13,18 @@ mkdir ~/.vim/colors
 cp ~/Documents/Vitrified-Code/Vim/twilight.vim ~/.vim/colors/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+## Ubuntu GUI
 # compiz config settings manager
 sudo apt install compizconfig-settings-manager python-compizconfig -y
-python ~/Documents/Vitrified-Code/Shell/import_compiz_settings.py ~/Documents/Vitrified-Code/Shell/compiz.profile
+python ~/Documents/Vitrified-Code/Ubuntu/import_compiz_settings.py ~/Documents/Vitrified-Code/Ubuntu/compiz.profile
+# Enable workspaces
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 2
+# Oneclick extract
+sudo apt install p7zip-full
+sudo cp ~/Documents/Vitrified-Code/Ubuntu/oneclickextract.desktop /usr/share/applications/
 
 # bashrc
-vimdiff ~/.bashrc ~/Documents/Vitrified-Code/Shell/bashrc
+vimdiff ~/.bashrc ~/Documents/Vitrified-Code/Ubuntu/bashrc
 
 # TODO ctrl+windows+uiojklm,. for moving windows
