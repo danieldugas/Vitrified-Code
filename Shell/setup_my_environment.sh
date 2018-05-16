d3a31f51f15b4e4c1d35da93dba1aa91e7e05d7e
Pepper <exodaniel@gmail.com>
2018-05-16 13:50:15 +0200

    Ubuntu environment setup

---
+++ Shell/setup_my_environment.sh
@@ -0,0 +1,20 @@
+# git config
+git config --global user.email "exodaniel@gmail.com"
+
+# git clone git@github.com:danieldugas/Vitrified-Code.git
+git clone git@github.com:danieldugas/pyniel.git ~/Documents/pyniel
+
+# Vim
+sudo apt install vim-gtk3
+cp ~/Documents/Vitrified-Code/Vim/vimrc ~/.vimrc
+mkdir ~/.vim/colors
+cp ~/Documents/Vitrified-Code/Vim/twilight.vim ~/.vim/colors/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# compiz config settings manager
+sudo apt install compizconfig-settings-manager
+
+# bashrc
+vimdiff ~/.bashrc ~/Documents/Vitrified-Code/Shell/bashrc
+
+# TODO ctrl+windows+uiojklm,. for moving windows
