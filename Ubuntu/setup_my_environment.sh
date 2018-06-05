8094294b13a4c65c786d5ac951b52c77ff0f6830
Daniel <exodaniel@gmail.com>
2018-06-05 18:38:53 +0200

    setup_my_environment.sh update

--- Ubuntu/setup_my_environment.sh
+++ Ubuntu/setup_my_environment.sh
@@ -4,11 +4,9 @@ git config --global user.email "exodaniel@gmail.com"
 # git clone git@github.com:danieldugas/Vitrified-Code.git
 git clone https://github.com/danieldugas/pyniel.git ~/Documents/pyniel
 
-
-
 # Vim
 sudo apt install vim-gtk3 -y
-cp ~/Documents/Vitrified-Code/Vim/vimrc ~/.vimrc
+ln ~/Documents/Vitrified-Code/Vim/vimrc ~/.vimrc
 mkdir ~/.vim/colors
 cp ~/Documents/Vitrified-Code/Vim/twilight.vim ~/.vim/colors/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -25,6 +23,13 @@ sudo apt install p7zip-full
 sudo cp ~/Documents/Vitrified-Code/Ubuntu/oneclickextract.desktop /usr/share/applications/
 
 # bashrc
-vimdiff ~/.bashrc ~/Documents/Vitrified-Code/Ubuntu/bashrc
+ln ~/Documents/Vitrified-Code/Ubuntu/bashrc ~/.bashrc_ext
+if [ -z "$MACHINE_NAME" ]; then
+  echo "Name of this machine: "
+  read machine_name
+  echo "# Added by setup_my_environment.sh script (Daniel)" >> ~/.bashrc
+  echo "export MACHINE_NAME=$machine_name" >> ~/.bashrc
+  echo "source ~/.bashrc_ext" >> ~/.bashrc
+fi
 
 # TODO ctrl+windows+uiojklm,. for moving windows
