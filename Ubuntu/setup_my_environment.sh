2dd9bc7d33d3803efe81376b98b7f91ae8ff2255
Daniel <exodaniel@gmail.com>
2018-06-27 14:20:39 +0200

    Gnome + autoupdated vitrified_code

--- Ubuntu/setup_my_environment.sh
+++ Ubuntu/setup_my_environment.sh
@@ -12,16 +12,6 @@ mkdir ~/.vim/colors
 cp ~/Documents/Vitrified-Code/Vim/twilight.vim ~/.vim/colors/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-## Ubuntu GUI
-# compiz config settings manager
-sudo apt install compizconfig-settings-manager python-compizconfig -y
-python ~/Documents/Vitrified-Code/Ubuntu/import_compiz_settings.py ~/Documents/Vitrified-Code/Ubuntu/compiz.profile
-# Enable workspaces
-gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
-gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 2
-# Oneclick extract
-sudo apt install p7zip-full
-sudo cp ~/Documents/Vitrified-Code/Ubuntu/oneclickextract.desktop /usr/share/applications/
 
 # bashrc
 ln ~/Documents/Vitrified-Code/Ubuntu/bashrc ~/.bashrc_ext
@@ -33,4 +23,21 @@ if [ -z "$MACHINE_NAME" ]; then
   echo "source ~/.bashrc_ext" >> ~/.bashrc
 fi
 
+## Ubuntu GUI
+# compiz config settings manager
+# sudo apt install compizconfig-settings-manager python-compizconfig -y
+# python ~/Documents/Vitrified-Code/Ubuntu/import_compiz_settings.py ~/Documents/Vitrified-Code/Ubuntu/compiz.profile
+# Enable workspaces
+# gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
+# gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 2
+# Gnome Extensions (TODO)
+# Keybindings
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Primary><Super>h']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Primary><Super>;']"
+# Behavior
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
+# Oneclick extract
+sudo apt install p7zip-full
+sudo cp ~/Documents/Vitrified-Code/Ubuntu/oneclickextract.desktop /usr/share/applications/
 # TODO ctrl+windows+uiojklm,. for moving windows
