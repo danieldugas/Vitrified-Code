fe2456174e1d71baba53e7713c8cdc4e96fa0099
Daniel Dugas <exodaniel@gmail.com>
2026-01-17 08:54:58 -0800

    vimrc server

--- Ubuntu/setup_my_server_environment.sh
+++ Ubuntu/setup_my_server_environment.sh
@@ -21,14 +21,13 @@ git config --global core.hooksPath ~/Code/Vitrified-Code/Ubuntu/git-hooks
 
 
 echo "------- SETTING UP VIM ------------"
-ln -s -i ~/Code/Vitrified-Code/Vim/vimrc ~/.vimrc
+ln -s -i ~/Code/Vitrified-Code/Vim/vimrc_server ~/.vimrc
 mkdir -p ~/.config
 ln -s -i ~/Code/Vitrified-Code/Ubuntu/flake8_config ~/.flake8
 mkdir -p ~/.vim/colors
 cp ~/Code/Vitrified-Code/Vim/twilight.vim ~/.vim/colors/
-mkdir -p ~/.config/nvim/colors
-cp ~/Code/Vitrified-Code/Vim/twilight.vim ~/.config/nvim/colors/
-# git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 # vim-plug
 
 echo "------- SETTING UP VSCODE ----------"
